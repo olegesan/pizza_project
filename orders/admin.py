ff973f644bb9a58f052716fd72304baa13f70ec7
olegesan <37033572+olegesan@users.noreply.github.com>
2019-11-05 21:47:51 -0500

    models are added and working, still require more thought of organization

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Pizza, Topping, Size,Sub, SubType, PizzaType, Menu
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Size)
+admin.site.register(Sub)
+admin.site.register(SubType)
+admin.site.register(PizzaType)
+admin.site.register(Menu)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+''' 
+general models
+'''
+class Menu(models.Model):
+    options = (('Y', 'Yes'),('N',"No"))
+    listed = models.CharField(choices=options, max_length=5)
+    def __str__(self):
+        return f'{self.listed}'
+class Size(models.Model):
+    size = models.CharField(max_length=64)
+    def __str__(self):
+        return self.size
+
+''' 
+pizza related section
+'''
+class PizzaType(models.Model):
+        pizza_type = models.CharField(max_length=128)
+        def __str__(self):
+            return self.pizza_type
+class Topping(models.Model):
+    topping = models.CharField(max_length=128)
+    def __str__(self):
+        return f'{self.topping}'
+class Pizza(models.Model):
+    pizza_type = models.ForeignKey(PizzaType, on_delete=models.CASCADE, related_name='pizza_types')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE,related_name='pizza_sizes')
+    toppings = models.ManyToManyField(Topping, blank=True, related_name='topings')
+    price = models.IntegerField()
+    on_the_menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu_pizzas')
+    def __str__(self):
+        return f'{self.size} {self.pizza_type} pizza with {self.toppings} topings for ${self.price} dollars'
+'''
+sub related models
+'''
+class SubType(models.Model):
+        sub_type = models.CharField(max_length=128)
+        def __str__(self):
+            return self.sub_type
+class Sub(models.Model):
+    sub_type = models.ForeignKey(SubType, on_delete=models.CASCADE, related_name='sub_types')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE,related_name='sub_sizes')
+    price = models.IntegerField()
+    listed = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu_subs')
+    def __str__(self):
+        return f'{self.size} {self.sub_type} for {self.price}'
