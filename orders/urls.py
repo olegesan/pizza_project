b8e7ea93340986ea5f793fb5fad827cf8fd08aaa
olegesan <37033572+olegesan@users.noreply.github.com>
2019-11-10 22:58:43 -0500

    added signup logic

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name = "index"),
     path("menu/", views.menu, name = "not_a_menu"),
-    path('somewhere/', views.menu, name = 'somewhere')
+    path('somewhere/', views.menu, name = 'somewhere'),
+    path('signup/', views.signup, name = 'signup' ),
+    path('signup/new/', views.signup_new, name = 'signup_new')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Size, MenuItem, Category, Kind
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
@@ -16,3 +19,19 @@ def menu(request):
     return render(request, "orders/menu.html", content)
 def somewhere(request):
     return HttpResponse('Somewhere')
+def signup(request):
+    return render(request,'orders/signup.html')
+def signup_new(request):
+    try:
+        # print(reuqest.POST['email'])
+        username = request.POST['login']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username,
+                                    email,
+                                    password)
+        user.save()
+    except:
+        # print(email, password, username)
+        return HttpResponse('there was an error')
+    return HttpResponseRedirect('/')
